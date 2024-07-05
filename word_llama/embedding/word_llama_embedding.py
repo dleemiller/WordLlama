97838936ba966b41cd007f9c472163e2ba9ced4e
Lee Miller <dleemiller@protonmail.com>
2024-07-05 16:49:53 -0600

    adding packing and hamming similarity methods

--- word_llama/embedding/word_llama_embedding.py
+++ word_llama/embedding/word_llama_embedding.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch import nn
 import safetensors.torch
@@ -55,21 +56,54 @@ class WordLlamaEmbedding(nn.Module):
         return self.tokenizer(texts, **self.tokenizer_kwargs)
 
     @torch.inference_mode()
-    def embed(self, texts: Union[str, list[str]], norm: bool = False, binarize=None):
+    def embed(
+        self,
+        texts: Union[str, list[str]],
+        norm: bool = False,
+        binarize: bool = False,
+        pack: bool = True,
+    ) -> np.array:
+        """Tokenize and embed a string or list of strings"""
+        single_text = False
         if isinstance(texts, str):
+            single_text = True
             self.tokenizer_kwargs["return_attention_mask"] = False
             self.tokenizer_kwargs["padding"] = "do_not_pad"
         else:
             self.tokenizer_kwargs["return_attention_mask"] = True
             self.tokenizer_kwargs["padding"] = "max_length"
+
+        # tokenize
         tensors = self.tokenize(texts)
+
+        # create embedding
         with torch.no_grad():
             x = self.embedding(tensors["input_ids"].to(self.embedding.weight.device))
             x = AvgPool.avg_pool(x, tensors.get("attention_mask"), norm=norm)
+            x = x.cpu().numpy()
 
         if binarize:
-            return x > 0
+            x = x > 0
+            if pack:
+                x = np.packbits(x, axis=-1)
+
+        if single_text:
+            x = x[0]
         return x
+
+    @staticmethod
+    def hamming_similarity(a, b):
+        assert a.shape == b.shape
+        assert a.ndim == 1
+        assert a.dtype == np.uint8
+        assert b.dtype == np.uint8
+
+        max_dist = a.size * 8
+
+        # calculate distance
+        dist = sum(bin(x).count("1") for x in a ^ b)
+
+        return 1.0 - dist / max_dist
 
     def save(self, *args, **kwargs):
         pass

295d07f85c71dd210fa7dfcf53b87a6fd0590f52
Lee Miller <dleemiller@protonmail.com>
2024-07-11 20:35:15 -0600

    moving out of setup

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, Extension, find_packages
 
+# Setup configuration
 setup(
     name="wordllama",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["numpy", "safetensors", "tokenizers", "toml"],
-    extras_require={
-        "train": ["torch", "transformers", "sentence-transformers", "datasets"]
-    },
     entry_points={
         "console_scripts": [
             "wordllama=wordllama.wordllama:main",
@@ -15,8 +12,7 @@ setup(
     },
     include_package_data=True,
     package_data={
-        # Include all toml files in the package
-        "": ["*.toml"]
+        "wordllama": ["**/*.toml"]
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -25,3 +21,4 @@ setup(
     ],
     python_requires=">=3.6",
 )
+

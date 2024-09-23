e26a8f79de806a266154ca6578c59e34846c41de
Lee Miller <dleemiller@protonmail.com>
2024-09-22 20:45:51 -0600

    adding __all__

--- wordllama/adapters/__init__.py
+++ wordllama/adapters/__init__.py
@@ -4,3 +4,12 @@ from .mlp import MLP
 from .weighted_mlp import WeightedMLP
 from .weighted_projector import WeightedProjector
 from .binarizer import Binarizer
+
+__all__ = [
+    "AvgPool",
+    "Projector",
+    "MLP",
+    "WeightedMLP",
+    "WeightedProjector",
+    "Binarizer"
+]

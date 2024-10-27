cfe4de13b4027dad60fb059c6e4d230241dbd65c
Lee Miller <dleemiller@protonmail.com>
2024-10-27 15:00:48 -0600

    use numpy bitwise count, require numpy>=2

--- setup.py
+++ setup.py
@@ -5,31 +5,8 @@ import platform
 
 numpy_include = np.get_include()
 
-extra_compile_args = []
+extra_compile_args = ["-O3", "-ffast-math"]
 extra_link_args = []
-
-if platform.system() == "Darwin":
-    if platform.machine() == "arm64":
-        extra_compile_args.extend(["-arch", "arm64", "-O3", "-ffast-math"])
-        extra_link_args.extend(["-arch", "arm64"])
-    else:
-        extra_compile_args.extend(["-arch", "x86_64", "-O3", "-ffast-math"])
-        extra_link_args.extend(["-arch", "x86_64"])
-elif platform.system() == "Windows":
-    extra_compile_args.extend(["/O2"])
-else:  # Linux and others
-    if platform.machine().startswith("arm"):
-        if platform.architecture()[0] == "32bit":
-            extra_compile_args.extend(["-march=armv7-a", "-mfpu=neon"])
-            extra_link_args.extend(["-march=armv7-a", "-mfpu=neon"])
-        else:  # 64-bit ARM
-            extra_compile_args.extend(["-march=armv8-a"])
-            extra_link_args.extend(["-march=armv8-a"])
-    elif platform.machine() in ["x86_64", "AMD64"]:
-        extra_compile_args.extend(["-march=native", "-mpopcnt"])
-        extra_link_args.extend(["-march=native", "-mpopcnt"])
-
-extra_compile_args.extend(["-O3", "-ffast-math"])
 
 extensions = [
     Extension(
@@ -61,7 +38,7 @@ extensions = [
         define_macros=[],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
-        language="c++"
+        language="c++",
     ),
     Extension(
         "wordllama.algorithms.find_local_minima",
@@ -70,7 +47,7 @@ extensions = [
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
-        language="c++"
+        language="c++",
     ),
     Extension(
         "wordllama.algorithms.vector_similarity",
@@ -79,15 +56,13 @@ extensions = [
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
-    )
-
-
+    ),
 ]
 
 setup(
     name="Embedding and lightweight NLP utility.",
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     ext_modules=cythonize(
         extensions,
         compiler_directives={
